6c8d4a5cf0d42649b9023acb649f128c3cef6695
jamebozo <jamebozo@gmail.com>
2020-11-01 13:48:24 +0800

    v1.1 finish add rest of code

--- 01_int_without_switch_or_if.c
+++ 01_int_without_switch_or_if.c
@@ -1,5 +1,7 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /*
  * Example: 
  *     if (ii == 1) func1();
@@ -8,18 +10,41 @@
  *     if (ii == 4) func4();
  * */
 ////////////////////////////
-void f1() 
+void f1(void) {
     printf("f1\n");
-void f2() 
+}
+void f2(void) {
     printf("f2\n");
-void f3() 
-    printf("f3\n");
-void f4() 
-    printf("f4\n");
+}
 
+void f3(void) { 
+    printf("f3\n");
+}
+void f4(void) {
+    printf("f4\n");
+}
+
+
+///////////////////////////
 void main(void) {
     printf("hello\n");
-    void *func[5];
+    void (*fp[5])();
+
+    f1();
+
+    int ii = 0;
+    //while (ii != -1) {
+    printf("Enter:\n");
+    scanf("%d", &ii);
+    printf("ii: %d\n", ii);
+
+    fp[0] = f1;
+    fp[1] = f2;
+    fp[2] = f3;
+    fp[3] = f4;
+    fp[ii-1]();
+    //    ii =0;
+    //}
 }
 
 ////////////////////////////
