2a46505396b0fe6dafb30a822a639b547674de3f
jamebozo <jamebozo@gmail.com>
2020-11-01 13:01:54 +0800

    v1.0 Init commit

---
+++ 01_int_without_switch_or_if.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+/*
+ * Example: 
+ *     if (ii == 1) func1();
+ *     if (ii == 2) func2();
+ *     if (ii == 3) func3();
+ *     if (ii == 4) func4();
+ * */
+////////////////////////////
+void f1() 
+    printf("f1\n");
+void f2() 
+    printf("f2\n");
+void f3() 
+    printf("f3\n");
+void f4() 
+    printf("f4\n");
+
+void main(void) {
+    printf("hello\n");
+    void *func[5];
+}
+
+////////////////////////////
+
